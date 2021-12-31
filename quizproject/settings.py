681f6681b66eff4714ee93dc084e40ab45c047d2
Vrushali Shah <vrushali.shah2710@gmail.com>
2021-12-31 13:28:45 +0530

    csrf cook

--- quizproject/settings.py
+++ quizproject/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'QuizApp',
 ]
-CSRF_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = False
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
